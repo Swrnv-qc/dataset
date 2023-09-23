4c32bbb0a302f872574f6c5a8830c809a4cec091
Swrnv_qc <56204621+Swrnv-qc@users.noreply.github.com>
2023-09-23 09:54:17 +0000

    add

--- count.py
+++ count.py
@@ -1,5 +1,5 @@
 import os
 
-path = '/path/to/folder'
+path = '/workspaces/dataset/New Plant Diseases Dataset(Augmented)/New Plant Diseases Dataset(Augmented)/Corn/train/Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot'
 num_files = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
 print(f'The number of files in {path} is {num_files}.')
